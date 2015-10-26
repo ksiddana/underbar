5caf6351000a3b49376e7c05fdc00e1f57ba6dcf
Karun Siddana <ksiddana@gmail.com>
2015-10-25 23:18:30 -0700

    Completed Part I funtions for Bookstrap

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,17 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var length = array.length;
+    
+    if (n === 0) {
+          return [];
+    } else if (n === undefined ) {
+          return array[length-1];
+    } else if (n > length) {
+          return array;
+    } else {
+          return array.slice(length-n, length);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +57,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +88,60 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    // console.log("Using the filter Function:")
+
+    _.each(collection, function (element) {
+      //console.log(element);
+      //console.log(test(element));
+        if (test(element)) {
+          result.push(element);
+        }
+    
+    
+    });
+    // console.log("Filtered Array ", result);
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    // console.log("--------------------------");
+    // console.log("Using the Reject Function:");
+    var rejectedCollection = [];
+    _.filter(collection, function (element) {
+      if (!test(element)) {
+        rejectedCollection.push(element);
+      }
+    });
+    // console.log("Rejected Collection: ", rejectedCollection);
+    return rejectedCollection;
+
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    
+    // console.log("Entering Unique Function:");
+    var result = {};
+    var unqiueArray = [];
+
+    _.each(array, function (item) {
+      //return unqiueArray[item] === undefined ? unqiueArray.push(item);
+      var temp = item;
+      if (result[temp] === undefined) {
+        result[temp] = 1;
+        unqiueArray.push(temp);
+      } else {
+        result[temp] += 1;
+      }
+    });
+    //console.log("Unqiue Array ", unqiueArray);
+    // console.log("Result of Unique Function ", result);
+    
+    return unqiueArray;
   };
 
 
@@ -84,6 +150,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function (element){
+      result.push(iterator(element));
+    });
+    return result;
   };
 
   /*
@@ -125,6 +196,23 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+      //console.log("----------------------------");
+      // console.log("Entering the Reduce Function");
+    _.each(collection, function(item) {
+      
+      // console.log("item: ", item);
+      // console.log("accumulator: ", accumulator);
+      
+      if (accumulator === undefined) {
+          accumulator = item;
+          // console.log("Accumulator is Undefined: ", accumulator);
+      } else {
+        accumulator = iterator(accumulator, item);
+      }
+
+      // console.log("Total: ", accumulator);
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
